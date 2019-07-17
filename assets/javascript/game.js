b03b3b52e197647dd7b207601d55ec965302d6f2
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-07-16 22:57:32 -0400

    added more vars to js and document.ready

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,61 @@
-// app starts w/ player pick//
-// player vs enemy: set health, power//
-// list events in regards to play: click //
+$(document).ready(function () {
 
+// declaring vars for characters from html //
 
-// declaring objects for characters //
-var chars = [{
+var finnChar = $("#finnH");
+    var jakeChar = $("#jakeD");
+    var iceKingChar = $("#iceKing");
+    var lsPChar = $("#lsP");
+
+// declar var for all characters in an array of objects //
+
+var allChars = [{
     name: "finn",
     health: 100,
     attack: 15,
+    counter: 20,
 },
 {
     name: "jake",
     health: 120,
     attack: 18,
+    counter: 35,
 },
 {
     name: "iceking",
     health: 130,
     attack: 10,
+    counter: 5,
 },
 {
     name: "lsp",
     health: 150,
     attack: 20,
+    counter: 18,
 
 }]
 
-
+// user selecting character on click //
 var player = {};
-var chosenChar
-$(".chars").on("click", function(){
+var chosenChar;
 
-    
-    console.log($(this).attr("name"));
-    for(var i = 0; i < chars.length; i++){
+var chosenChar = "";
+var opponent = "";
+var currentE = "";
+var currentAttack = 0;
+var wins = 0;
+var restartBtn = $("<button>Restart</button>");
+var attackBtn = $('#attack-button" ATTACK!');
 
-        if ($(this).attr("name") === chars[i].name){
+
+
+$(".chars").on("click", function () {
+
+    console.log($(this).attr("name")); // logging the chars name list //
+
+    for (var i = 0; i < chars.length; i++) {
+
+        if ($(this).attr("name") === chars[i].name) {
             player.health = chars[i].health;
             printToScreen();
 
@@ -43,33 +63,48 @@ $(".chars").on("click", function(){
             console.log(chars[i].attack);
 
         }
-    
 
         console.log(chars[i].name);
-        
-    }
-})
 
-// let player = {
-//     health: 180,
-//     power: 20,
+    }
+});
+
+var enemy = {};
+var chosenEn;
+
+$(".chars").on("click", function() {
+
+    console.log($(this).attr("name"));
+
+    for (var j = 0; j < chars.length; j++) {
+
+        if($(this).attr("name") === chars[j].name) {
+            enemy.health = chars[j].health;
+            printToScreen();
+
+            console.log(chars[j].health);
+            console.log(chars[j].attack);
+        }
+    }
+
+
+
+});
+
+
+
+// const attack = () => {
+//     let playerAtk = Math.floor(Math.random() * player.power);
+//     enemy.health -= playerAtk;
+//     printToScreen();
 
 // }
-
-let enemy = {
-    health: 120,
-    power: 20
-}
-
-const attack = () => {
-    let playerAtk = Math.floor(Math.random() * player.power);
-    enemy.health -= playerAtk;
-    printToScreen();
-
-}
 
 const printToScreen = () => {
     document.getElementById('player-health').innerText = player.health;
     document.getElementById('enemy-health').innerText = enemy.health;
 
 }
+
+
+});
