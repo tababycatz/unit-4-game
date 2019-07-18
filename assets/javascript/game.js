355a749b516d2e1225c69a5f722102f0b4f6fd20
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-07-17 21:17:03 -0400

    edited html to add more divs and etc

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,40 +9,42 @@ $(document).ready(function () {
 
     // declar var for all characters in an array of objects //
 
-    var allChars = [{
-        name: "finn",
-        health: 100,
-        attack: 15,
-        counter: 20,
-        loseImg: "finnlose.png",
-    },
-    {
-        name: "jake",
-        health: 120,
-        attack: 18,
-        counter: 35,
-        loseImg: "jakelose.png",
-    },
-    {
-        name: "iceking",
-        health: 130,
-        attack: 10,
-        counter: 5,
-        loseImg: "icekinglose.png",
-    },
-    {
-        name: "lsp",
-        health: 150,
-        attack: 20,
-        counter: 18,
-        loseImg: "lsplose.png"
+    var allChars = {
+        finn: {
+            health: 100,
+            attack: 15,
+            counter: 20,
+            loseImg: "finnlose.png",
 
-    }]
+        },
+        jake: {
+            health: 120,
+            attack: 18,
+            counter: 35,
+            loseImg: "jakelose.png",
+        },
+        iceking: {
+
+            health: 130,
+            attack: 10,
+            counter: 5,
+            loseImg: "icekinglose.png",
+        },
+
+        lsp: {
+            health: 150,
+            attack: 20,
+            counter: 18,
+            loseImg: "lsplose.png"
+
+        }
+
+    };
 
     // listed all possible vars //
 
     var player = "";
-    var chosenChar = "";
+    var myChar = "";
     var opponent = "";
     var chosenEn = "";
     var currentAttack = 0;
@@ -52,24 +54,38 @@ $(document).ready(function () {
 
     // user selecting character on click //
 
-    $(".chars").on("click", function () {
+    $(".allChars").on("click", function () {
 
-        // if ()
-
-        for (var i = 0; i < allChars.length; i++) {
-
-            if ($(this).attr("name") === allChars[i].name) {
-                player.health = allChars[i].health;
+        if (!player) {
+            player = allChars[$(this).val()];
+            chosenChar();
+            player.health = allChars[this].health;
                 printToScreen();
 
-                console.log(allChars[i].health);
-                console.log(allChars[i].attack);
+            console.log(player.health);
 
-            }
 
-            console.log(allChars[i].name);
+        };
 
-        }
+        function chosenChar() {
+            $(".enemyS").append(finnChar, jakeChar, iceKingChar, lsPChar);
+
+        };
+
+        // for (var i = 0; i < allChars.length; i++) {
+
+        //     if ($(this).attr("name") === allChars[i].name) {
+
+        //         printToScreen();
+
+        //     console.log(allChars[i].health);
+        //     console.log(allChars[i].attack);
+
+        // }
+
+        // console.log(allChars[i].name);
+
+
 
     });
 
